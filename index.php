e7f4b2090105405c64942dbdee31c73f3409f4f9
Alex Sandro de Almeida <sanderghost@gmail.com>
2017-05-13 14:40:48 -0300

    Frist Commit
    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!-- Curso Ecommerce PHP MySQL
+https://www.youtube.com/watch?v=DVO99vc1YRM&index=1&list=PLB_Wd4-5SGAbYqmJ4OVF24g0lNFnUap-W -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Khan Store</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <scripts src="js/main.js"></script>
+  </head>
+
+  <body>
+<!-- MENU -->
+    <div class="navbar navbar-inverse navabar-fixed-top">
+
+      <div class="container-fluid">
+
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">Khan Store</a>
+
+        </div>
+          <ul class="nav navbar-nav">
+            <li>
+              <a href="#">
+                <span class="glyphicon glyphicon-home"></span>Home
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <span class="glyphicon glyphicon-modal-window"></span>Product
+              </a>
+            </li>
+
+            <li style="width:300px; left:10px; top:10px;">
+              <input type="text" class="form-control" id="search" name="" value="">
+            </li>
+
+            <li style="top:10px; left:20px;"><input type="submit" class="btn    btn-primary" id="search_btn" name="submit" value="Submit">
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart
+              </a>
+            </li>
+            <li>
+              <a href="#"><span class="glyphicon glyphicon-user"></span>Signin
+              </a>
+            </li>
+
+            <li>
+              <a href="#"><span class="glyphicon glyphicon-user"></span>Signup
+              </a>
+            </li>
+
+      </div>
+
+    </div>
+    <!-- END MENU -->
+
+    <p><br/></p>
+    <p><br/></p>
+    <p><br/></p>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-1">
+          <div class="col-md-2">
+            <div class="col-md-1">
+              <div class="col-md-1">
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
